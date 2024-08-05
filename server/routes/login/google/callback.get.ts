32b43f568ab4d287f825ec7d3d097cc8933c9a91
cblokkeel <cblokkeeltech@gmail.com>
2024-08-05 14:00:26 +0200

    remove console.log

--- server/routes/login/google/callback.get.ts
+++ server/routes/login/google/callback.get.ts
@@ -12,14 +12,12 @@ interface GoogleUser {
 }
 
 export default defineEventHandler(async (event) => {
-    console.log("CALLBACK")
     const query = getQuery(event);
     const code = query.code?.toString() ?? null;
     const state = query.state?.toString() ?? null;
     const storedState = getCookie(event, "google_oauth_state") ?? null;
     const storedCodeVerifier = getCookie(event, "code_verifier" ) ?? null;
     if (!code || !state || !storedState || state !== storedState || !storedCodeVerifier) {
-        console.log("WRONG")
         throw createError({
             status: 400,
         });
@@ -27,7 +25,6 @@ export default defineEventHandler(async (event) => {
 
     try {
         const tokens = await google.validateAuthorizationCode(code, storedCodeVerifier);
-        console.log(tokens)
         const googleResp = await fetch(
             "https://www.googleapis.com/oauth2/v1/userinfo?alt=json",
             {
@@ -40,13 +37,9 @@ export default defineEventHandler(async (event) => {
 
         const googleData: GoogleUser = await googleResp.json();
 
-        console.log(googleData)
-
         const existing = await User.findOne({
             google_id: googleData.id,
         });
-
-        console.log(existing)
 
         if (existing) {
             const session = await lucia.createSession(existing.id, {});
@@ -58,8 +51,6 @@ export default defineEventHandler(async (event) => {
             return sendRedirect(event, "/");
         }
 
-        console.log("GOOD")
-
         const userId = crypto.randomUUID();
 
         await User.create({
@@ -70,8 +61,6 @@ export default defineEventHandler(async (event) => {
             google_id: googleData.id,
         });
 
-        console.log("created")
-
         const session = await lucia.createSession(userId, {});
         appendHeader(
             event,
@@ -80,7 +69,6 @@ export default defineEventHandler(async (event) => {
         );
         return sendRedirect(event, "/");
     } catch (err) {
-        console.error("chatter", err)
         if (err instanceof OAuth2RequestError) {
             throw createError({
                 status: 400,
