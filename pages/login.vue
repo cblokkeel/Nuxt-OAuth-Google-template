127c3a61905ae746047a27e453715e75fad34f60
cblokkeel <cblokkeeltech@gmail.com>
2024-08-05 13:52:26 +0200

    add starter

---
+++ pages/login.vue
@@ -0,0 +1,15 @@
+<script setup>
+async function googleLogin() {
+    const {
+        data,
+    } = await useFetch("/login/google");
+    console.log(data.value);
+    navigateTo(data.value.url, {
+        external: true
+    })
+}
+</script>
+
+<template>
+    <UButton @click="googleLogin">Login with google</UButton>
+</template>
